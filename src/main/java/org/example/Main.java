6b6845174a7a373f77f99862669f4a51a316a949
juanbebr <jberrio@sura.com.co>
2024-09-13 14:00:13 -0500

    cambio en el comentario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,15 +9,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        /*
-        * Plantear un programa que ejecute DOS HILOS de programa al mismo tiempo, para esto se requiere que uno de los hilos realice
-        *  un conteo regresivo temporizado partiendo de un número establecido por el usuario, la temporización entre cada ejecución del hilo
-        * debe ser de 600 ms, el otro hilo, debe mostrar las letras del alfabeto partiendo desde la A hasta una letra ingresada por el usuario,
-        *  este hilo debe estar temporizado a 500 ms.
-        * Ambos hilos deben detenerse cuando el hilo numero 1 encuentre la letra ingresada por el usuario.
-        * Al ejecutar el programa en consola se debe evidenciar la salida entregada por ambos hilos, al final de su ejecución
-        * cada hilo debe mostrar el mensaje “Trabajo del hilo [nombre] terminado”.
-        * */
 
         ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
 
